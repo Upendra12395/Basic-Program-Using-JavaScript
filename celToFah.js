b66e4b62cf7c53722224ab63c011e12e9208d119
Upendra12395 <ch95upendra@gmail.com>
2021-08-05 22:19:19 +0530

    new commit

---
+++ celToFah.js
@@ -0,0 +1,9 @@
+function celTofah(celsius){
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
+}
+
+celTofah(55)
