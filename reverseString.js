66ef04d9c82defff9291cc1efab4fafff214432c
Upendra12395 <ch95upendra@gmail.com>
2021-08-08 22:57:47 +0530

    new commit

---
+++ reverseString.js
@@ -0,0 +1,15 @@
+// program to reverse a string
+
+function reverseString(str) {
+
+    // empty string
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+
+const result = reverseString("Hello World");
+console.log(result);
