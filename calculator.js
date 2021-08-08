a3831cf36ea82b0a4c6883b206494bcf490d2fa9
Upendra12395 <ch95upendra@gmail.com>
2021-08-08 22:51:17 +0530

    new commit

---
+++ calculator.js
@@ -0,0 +1,25 @@
+// program for a simple calculator
+
+function calculator(operator, number1, number2) {
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
+}
+
+calculator('-', 2, 4)
