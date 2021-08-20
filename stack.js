9600c81f24d635badd9e9a4777b2ae46c84b11fd
Upendra12395 <ch95upendra@gmail.com>
2021-08-20 23:58:54 +0530

    new commit

---
+++ stack.js
@@ -0,0 +1,54 @@
+// program to implement stack data structure
+const array = [];
+    
+    // add element to the stack
+function add(element) {
+        return array.push(element);
+}
+    
+    // remove element from the stack
+    function remove() {
+        if(array.length > 0) {
+            return array.pop();
+        }
+    }
+    
+    // view the last element
+    function peek() {
+        return array[array.length - 1];
+    }
+    
+    // check if the stack is empty
+    function isEmpty(){
+       return array.length == 0;
+    }
+   
+    // the size of the stack
+    function size(){
+        return array.length;
+    }
+ 
+    // empty the stack
+    function clear(){
+        array = [];
+    }
+
+
+
+add(1);
+add(2);
+add(4);
+add(8);
+console.log(array);
+
+remove();
+console.log(array);
+
+console.log(peek());
+
+console.log(isEmpty());
+
+console.log(size());
+
+clear();
+console.log(array);
