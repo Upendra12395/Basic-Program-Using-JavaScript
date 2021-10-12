eb80fce3c88b258321964651dc57e9e20816526d
Upendra12395 <ch95upendra@gmail.com>
2021-10-12 22:28:07 +0530

    exec method child process

---
+++ child_process/exec.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process')
+const { stderr } = require('process')
+
+// exec('pwd', (error, stdout, stderr)=>{
+//     if (error){
+//         console.log(`error : ${error}`)
+//         return
+//     }
+//     if (stderr){
+//         console.log(stderr)
+//         return
+//     }
+//     console.log(`stdout : ${stdout}`)
+// })
+
+exec('ls -lh', (error, stdout, stderr)=>{
+    if (error){
+        console.log(`error : ${error}`)
+        return
+    }
+    if (stderr){
+        console.log(stderr)
+        return
+    }
+    console.log(`stdout : ${stdout}`)
+})
